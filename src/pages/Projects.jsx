98d6f9bd52a5a30171b88da451a297fb1a0752c0
Manas <manasmahajan2003@yahoo.com>
2024-07-14 18:11:40 +0530

    Projects upload

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,22 +6,41 @@ const Projects = () => {
   return (
     <Container fluid>
       <div className='projects-heading-container'><h2 className='projects-heading'>My Projects</h2></div>
-      
       <Card className="text-center mb-3">
-        <Card.Header><b>Airbnb Clone</b></Card.Header>
+        <Card.Header><b>Ecommerce Website</b></Card.Header>
         <Card.Body>
-          <Card.Title>An Airbnb clone while learning about different backend concepts.</Card.Title>
-          <Card.Text>Implemented MVC design pattern.</Card.Text>
+          <Card.Title>A complete MERN stack project</Card.Title>
+          <Card.Text>Its my major project in which I invested most of the time and got a feel of working with excessive lines of code and redux toolkit. Incorporated various features like user authentication, admin features, CRUD operations of products, categories, users. Various other necessary features like favorites, cart, orders, shipping status, payment etc are also included.  </Card.Text>
+          <Button href="https://github.com/ManasMahajan2003/Ecommerce-Website" variant="primary" className="m-1">GitHub repository</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">React.js MongoDB Node.js Express.js</Card.Footer>
+      </Card>
+      <Card className="text-center mb-3">
+        <Card.Header><b>StaySphere</b></Card.Header>
+        <Card.Body>
+          <Card.Title>An Airbnb inspired website made while learning about different backend concepts.</Card.Title>
+          <Card.Text>It was my first big project in which I learned about user authentication, middlewares, error handling etc and understood about MVC design pattern.</Card.Text>
           <Button href="https://airbnb-clone-g9gj.onrender.com" variant="primary" className="m-1">View project</Button>
           <Button href="https://github.com/ManasMahajan2003/Wanderlust" variant="primary" className="m-1">GitHub repository</Button>
         </Card.Body>
-        <Card.Footer className="text-muted">Node.js  Express.js MongoDB</Card.Footer>
+        <Card.Footer className="text-muted">Node.js Express.js MongoDB</Card.Footer>
+      </Card>
+      <Card className="text-center mb-3">
+        <Card.Header><b>Expense Tracker App</b></Card.Header>
+        <Card.Body>
+          <Card.Title>Expense Tracking Application for daily usage</Card.Title>
+          <Card.Text>Utilized third party library "Chakra UI" to represent expenses and income in a user friendly way.</Card.Text>
+          <Button href="https://expense-tracker-app-three-azure.vercel.app/" variant="primary" className="m-1">View project</Button>
+          <Button href="https://github.com/ManasMahajan2003/Expense-Tracker-App" variant="primary" className="m-1">GitHub repository</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">React.js</Card.Footer>
       </Card>
       <Card className="text-center mb-3">
         <Card.Header><b>MERN Blog App</b></Card.Header>
         <Card.Body>
           <Card.Title>A MERN Stack Blog website</Card.Title>
-          <Card.Text>Focused on implementing CRUD operations</Card.Text>
+          <Card.Text>After learning React, I wanted to experience that how I can connect it with backend. So i made this project and focused on basic CRUD operations.</Card.Text>
+          <Button href="https://mern-blog-roan.vercel.app/" variant="primary" className="m-1">View project</Button>
           <Button href="https://github.com/ManasMahajan2003/MERN_BLOG" variant="primary" className="m-1">GitHub repository</Button>
         </Card.Body>
         <Card.Footer className="text-muted">React.js MongoDB Node.js Express.js</Card.Footer>
@@ -30,7 +49,7 @@ const Projects = () => {
         <Card.Header><b>Weather App</b></Card.Header>
         <Card.Body>
           <Card.Title>A weather app using APIs in React</Card.Title>
-          <Card.Text>Takes location input from user and display weather with good UX design</Card.Text>
+          <Card.Text> This project was made while learning React which takes location input from user and display weather with good UX design. </Card.Text>
           <Button href="https://weather-application-gray-phi.vercel.app/" variant="primary" className="m-1">View project</Button>
           <Button href="https://github.com/ManasMahajan2003/Weather-Application" variant="primary" className="m-1">GitHub repository</Button>
         </Card.Body>
@@ -40,7 +59,7 @@ const Projects = () => {
         <Card.Header><b>Food Recipe App</b></Card.Header>
         <Card.Body>
           <Card.Title>React app for fetching food recipes from an API</Card.Title>
-          <Card.Text>Takes user input and gets recipe result, "Add to Favorites" feature included</Card.Text>
+          <Card.Text>Takes user input and gets recipe result, Learned about "Add to Favorites" feature.</Card.Text>
           <Button href="https://food-recipe-app-pi-ten.vercel.app/" variant="primary" className="m-1">View project</Button>
           <Button href="https://github.com/ManasMahajan2003/FoodRecipe-App" variant="primary" className="m-1">GitHub repository</Button>
         </Card.Body>
@@ -49,13 +68,24 @@ const Projects = () => {
       <Card className="text-center mb-3">
         <Card.Header><b>Shopping Cart</b></Card.Header>
         <Card.Body>
-          <Card.Title>Fetched products from an API and added "Add to cart" feature</Card.Title>
-          <Card.Text>Motive of the project was to work with redux-toolkit and learn about cart features</Card.Text>
+          <Card.Title>React project utilizing Redux Toolkit</Card.Title>
+          <Card.Text>The motive of this project was to work with redux-toolkit and learn about cart features</Card.Text>
           <Button href="https://shopping-cart-seven-ashy.vercel.app/" variant="primary" className="m-1">View project</Button>
           <Button href="https://github.com/ManasMahajan2003/ShoppingCart" variant="primary" className="m-1">GitHub repository</Button>
         </Card.Body>
         <Card.Footer className="text-muted">React.js</Card.Footer>
       </Card>
+      <Card className="text-center mb-3">
+        <Card.Header><b>Desktop Spotify clone</b></Card.Header>
+        <Card.Body>
+          <Card.Title>CSS Project of Desktop Spotify</Card.Title>
+          <Card.Text>Basic project made during early days of learning HTML, CSS and Javascript</Card.Text>
+          <Button href="https://manasmahajan2003.github.io/Spotify-clone/" variant="primary" className="m-1">View project</Button>
+          <Button href="https://github.com/ManasMahajan2003/Spotify-clone" variant="primary" className="m-1">GitHub repository</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">HTMl CSS</Card.Footer>
+      </Card>
+      
     </Container>
   );
 };
